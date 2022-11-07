f8e19c90b34ec24f88c356576c7d22f68113e1c3
Victor <victorjuanrazer@gmail.com>
2022-11-07 14:28:54 -0300

    add: Card

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ function App() {
     <div className="App">
       <h1>Ex React Redux</h1>
       <div className='linha'>
-        <Card></Card>
+        <Card title='Card1' red>X</Card>
       </div>
       <div className='linha'>
-        <Card></Card>
+      <Card title='Card2' green>X</Card>
+        <Card title='Card3' blue>Y</Card>
+        <Card title='Card4' purple>Y</Card>
       </div>
     </div>
   );
