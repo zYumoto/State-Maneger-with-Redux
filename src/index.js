caa2c44ece6e259b489ac31190a99ee6fd35d9d9
Victor <victorjuanrazer@gmail.com>
2022-11-07 15:49:04 -0300

    REDUX CONFIG

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from 'react-redux'
+import configStore from './store/storeConfig'
+
+const store = configStore()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
+
+
     <App />
   </React.StrictMode>
+  </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
